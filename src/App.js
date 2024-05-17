217911a4bdbfaf13f7b4d7a708c7c6c03893f683
ROZZNN <av5gust.slap@yandex.ru>
2024-05-17 20:04:08 +0300

    update site structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-// import '@babel/plugin-proposal-private-property-in-object'
-import Header from './headerr/header.js'
-// import Sliderforum from './general-page/forum-said-slider/sider-forum.js'
-// import Sliderclass from './general-page/forum-said-slider/sider-class.js'
-// import News from "./general-page/news-block/news-all.js"
-// import About from "./About/about.js"
-import Registration from './regitration-page/registration.js'
+import '@babel/plugin-proposal-private-property-in-object'
+import Header from './Common blocks/headerr/header.js'
+import Sliderforum from './general-page/forum-said-slider/sider-forum.js'
+import Sliderclass from './general-page/forum-said-slider/sider-class.js'
+import News from "./general-page/news-block/news-all.js"
+import About from "./Common blocks/About/about.js"
+import Registration from './Users/regitration-page/registration.js'
 import './bg.css'
 import './App.css'
 function App() {
   return (
     <div class="forum">
-      
     </div>
     // <div id='regitration'>
     //   <Header/>
@@ -24,7 +23,7 @@ function App() {
       //     <Sliderclass/>
       //   </div>
       //   <About/>
-      // </div>
+    // </div>
   );
 
 }
