17af09c123825adc212089400a65b177c9b877d9
ROZZNN <av5gust.slap@yandex.ru>
2024-05-18 16:01:01 +0300

    ADD_ALL_PAGE_STRUCTURE)

--- src/App.js
+++ src/App.js
@@ -5,16 +5,48 @@ import Sliderclass from './general-page/forum-said-slider/sider-class.js'
 import News from "./general-page/news-block/news-all.js"
 import About from "./Common blocks/About/about.js"
 import Registration from './Users/regitration-page/registration.js'
+import ForumQuest from './forum/Discusion-page/All-Discusion.js'
+import Comment from './forum/selected question/commet.js'
+import Profil from './forum/selected question/Profil-quest.js'
+import AddSubject from "./forum/Add-question/text-block.js"
+import Profil from './Users/Profil/Profil.js'
+import ClassTypeForm from './ClassSelect/Class-type-form.js'
+
 import './bg.css'
 import './App.css'
 function App() {
   return (
-    <div class="forum">
-    </div>
+    
+    // <div class="Class-type">
+    //   <ClassTypeForm/>
+    // </div>
+
+    // <div class="profil">
+    //   <Header/>
+    //   <Profil/>
+    //   <About/>
+    // </div>
+
+    // <div class="forum">
+    //   <Header/>
+    //   <div class="allsubject">
+    //     <ForumQuest/>
+    //   </div>
+    //   <div class="selectSubject" style="display:none" >
+    //       <Profil/>
+    //       <Comment/>
+    //   </div>
+    //   <div class="AddSubject" style="display:none">
+    //     <AddSubject/>
+    //   </div>
+    //   <About/>
+    // </div>
+
     // <div id='regitration'>
     //   <Header/>
     //   <Registration/>
     // </div>
+
       // <div id='general'>
       //   <Header/> 
       //   <div class='flex-container'>
